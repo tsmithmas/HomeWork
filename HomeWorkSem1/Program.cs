0d185f452eefe9c09d56cf44e523a95a3ef57591
Alex <16alextancura@gmail.com>
2023-07-07 21:47:33 +0300

    Добавил задачу 6 и начал задачу 8

--- HomeWorkSem1/Program.cs
+++ HomeWorkSem1/Program.cs
@@ -25,34 +25,61 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.Write("Введите первое число: ");
+// Console.Write("Введите первое число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int numberB = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите третье число: ");
+// int numberC = Convert.ToInt32(Console.ReadLine());
+
+// if(numberA > numberB)
+// {
+//     if(numberA > numberC)
+//     {
+//          Console.Write(numberA + " самое большое число! ");
+//     }
+//     else
+//     {
+//          Console.Write(numberB + " самое большое число! ");
+//     }
+// }
+// else
+// {
+//     if(numberB > numberC)
+//     {
+//          Console.Write(numberB + " самое большое число! ");
+//     }
+//     else
+//     {
+//          Console.Write(numberC + " самое большое число! ");
+//     }
+// }
+
+// Задача 6: Напишите программу, которая на вход принимает число 
+// и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+// Console.Write("Введите число: ");
+// int numberA = Convert.ToInt32(Console.ReadLine());
+
+// if(numberA % 2==1)
+// {
+//     Console.WriteLine(numberA + " нечетное число!");
+// }
+// else
+// {
+//     Console.WriteLine(numberA + " четное число!");
+// }
+
+// Задача 8: Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
 int numberA = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Введите второе число: ");
-int numberB = Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите третье число: ");
-int numberC = Convert.ToInt32(Console.ReadLine());
-
-if(numberA > numberB)
-{
-    if(numberA > numberC)
-    {
-         Console.Write(numberA + " самое большое число! ");
-    }
-    else
-    {
-         Console.Write(numberB + " самое большое число! ");
-    }
-}
-else
-{
-    if(numberB > numberC)
-    {
-         Console.Write(numberB + " самое большое число! ");
-    }
-    else
-    {
-         Console.Write(numberC + " самое большое число! ");
-    }
-}
