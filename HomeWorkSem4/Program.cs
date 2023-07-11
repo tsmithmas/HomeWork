4d9dd2ef280fe978ca52096cd185ef088e412394
Alex <16alextancura@gmail.com>
2023-07-11 16:33:43 +0300

    добавил  HomeWorkSem4 и выполнил в нем 25, 27 и 29ю задачу

---
+++ HomeWorkSem4/Program.cs
@@ -0,0 +1,112 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+// int Prompt(string messege)
+// {
+//     Console.Write(messege);
+//     string readInput = Console.ReadLine();
+//     int result = int.Parse(readInput);
+//     return result;
+// }
+
+// int Power(int powerBase, int exponent)
+// {
+//     int power = 1;
+//     for (int i = 0; i < exponent; i++)
+//     {
+//         power *= powerBase;
+//     }
+//     return power;
+// }
+
+// bool ValidateExponent(int exponent)
+// {
+//     if (exponent < 0)
+//     {
+//         Console.WriteLine("Показатель не должен быть меньше нуля");
+//         return false;
+//     }
+//     return true;
+// }
+
+// int powerBase = Prompt("Введите основание: ");
+// int exponent = Prompt("Введите показатель: ");
+// if (ValidateExponent(exponent))
+// {
+//     Console.WriteLine("Число " + powerBase + " в степени " + exponent + " равно: " + Power(powerBase, exponent));
+
+// }
+
+
+// Задача 27: Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+// int Prompt(string messege)
+// {
+//     Console.Write(messege);
+//     string readInput = Console.ReadLine();
+//     int result = int.Parse(readInput);
+//     return result;
+// }
+
+// int SumAllDigit(int number)
+// {
+//     int result = 0;
+//     while (number > 0)
+//     {
+//         result += number % 10;
+//         number = number / 10;
+//     }
+//     return result;
+// }
+
+// int number = Prompt("Введите число: ");
+// Console.WriteLine("Сумма всех цифр в числе " + number + " = " + SumAllDigit(number));
+
+
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов 
+// в диапазоне от 10 до 1000 и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int Prompt(string messege)
+{
+    Console.Write(messege);
+    string readInput = Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+} 
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        Console.Write(array[array.Length] + ",");
+    }
+    Console.Write(array[array.Length - 1]);
+    Console.WriteLine("]");
+}
+
+int length = Prompt("Длина массива: ");
+int min = Prompt("Начальное значение, для диапазона случайного числа: ");
+int max = Prompt("Конечное значение, для диапазона случайного числа: ");
+int[] array = GenerateArray(length, min, max);
+
+// Последняя задача не очень работает=(
